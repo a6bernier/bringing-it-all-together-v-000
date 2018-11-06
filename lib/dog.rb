3bbc5e6e117522e2d60d7ad2ca9f88c1d6f5474b
Andrew Bernier <a6bernier@gmail.com>
2018-11-06 23:00:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,9 @@ class Dog
 
   attr_accessor :name, :breed, :id
 
-  def initialize(name, breed, id = nil)
-    @breed = breed
-    @name = name
-    @id = id
+  def initialize(hash)
+    @name = hash[:name]
+    @breed = hash[:breed]
   end
 
 
